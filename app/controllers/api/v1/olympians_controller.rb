a3c75cf86f92720630023fa2bea854779c403be4
Christopher Kelling <cj.kelling88@gmail.com>
2020-03-03 11:53:20 -0700

    Edit index controller to search for params of age with a value of youngest.

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    olympian_data = Olympian.all
-    data = olympian_data.map { |olympian| OlympianSerializer.new(olympian)}
-    render json: data
+    if(params.has_key?(:age) && params.has_value?('youngest'))
+      render json: OlympianSerializer.new(Olympian.youngest)
+    else
+      olympian_data = Olympian.all
+      data = olympian_data.map { |olympian| OlympianSerializer.new(olympian)}
+      render json: data
+    end
   end
 end
