ced752c2b3d0e397fd03a315f993c50468b026db
Christopher Kelling <cj.kelling88@gmail.com>
2020-03-03 12:01:22 -0700

    Create test to find oldest Olympian

--- spec/requests/api/v1/features/get_oldest_spec.rb
+++ spec/requests/api/v1/features/get_oldest_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-describe 'Youngest Olympian Data Request' do
+describe 'Oldest Olympian Data Request' do
   it 'sends data for youngest Olympian' do
     olympian = create(:olympian)
     medal = create(:medal, olympian_id: olympian.id)
 
-    olympian_2 = create(:olympian, name: 'Ana Iulia Dascl', age: 13)
+    olympian_2 = create(:olympian, name: 'Julie Brougham', age: 62)
     medal_2 = create(:medal, olympian_id: olympian_2.id, medal: 'NA')
 
-    get '/api/v1/olympians?age=youngest'
+    get '/api/v1/olympians?age=oldest'
 
     expect(response).to be_successful
 
     olympians = JSON.parse(response.body)
 
-    expect(olympians['name']).to eq('Ana Iulia Dascl')
+    expect(olympians['name']).to eq('Julie Brougham')
     expect(olympians['team']).to eq('Egypt')
-    expect(olympians['age']).to eq(13)
+    expect(olympians['age']).to eq(62)
     expect(olympians['sport']).to eq('Diving')
     expect(olympians['total_medals_won']).to eq(0)
   end
