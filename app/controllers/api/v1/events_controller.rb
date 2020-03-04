1adf7942e9dbf852a7b7a20310f5c51855716028
Christopher Kelling <cj.kelling88@gmail.com>
2020-03-03 18:22:11 -0700

    Create controller action to find medalist based on event id

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::EventsController < ApplicationController
   def index
-    render json: Sport.all.map { |sport| EventSerializer.new(sport)}
+    render json: Sport.all.map { |sport| EventSerializer.new(sport) }
+  end
+
+  def medalists
+    render json: EventMedalistSerializer.new(Event.find(params[:id]))
   end
 end
